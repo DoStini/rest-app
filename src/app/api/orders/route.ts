0f260aa15efd0dac35619298f957231ff861b7d1
André Moreira <andre.moreira.9@hotmail.com>
2023-07-23 16:43:27 +0100

    Hotfix API not dynamic

--- src/app/api/orders/route.ts
+++ src/app/api/orders/route.ts
@@ -1,7 +1,7 @@
 import { TableController } from "@/controllers/TableControllers";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest) {
+export async function GET(request: Request) {
   const activeTables = await TableController.findActiveTables();
   console.log("GET", activeTables);
   return NextResponse.json({
