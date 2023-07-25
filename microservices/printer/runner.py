9b9ed958cf106797ebd86f5a73842e850d83b507
André Moreira <andre.moreira.9@hotmail.com>
2023-07-25 23:00:18 +0100

    Format python outputs

--- microservices/printer/runner.py
+++ microservices/printer/runner.py
@@ -1,6 +1,7 @@
 from confluent_kafka import Consumer
 import json
 
+
 def read_ccloud_config(config_file):
     conf = {}
     with open(config_file) as fh:
@@ -11,6 +12,7 @@ def read_ccloud_config(config_file):
                 conf[parameter] = value.strip()
     return conf
 
+
 props = read_ccloud_config(".env.local")
 props["group.id"] = "python-group-1"
 props["auto.offset.reset"] = "earliest"
@@ -18,13 +20,65 @@ props["auto.offset.reset"] = "earliest"
 consumer = Consumer(props)
 consumer.subscribe(["order-print"])
 
+# value coming from the topic
+# const waiter = order.creator;
+# const initialTime = order.createdAt;
+# const closeTime = order.closedAt;
+
+# const mappedOrder = order.OrderProduct.map((item) => {
+#   return {
+#     name: item.product.name,
+#     amount: item.amount,
+#     price: item.product.price,
+#     total: round2(round2(item.product.price) * item.amount),
+#   };
+# });
+
+# print data coming from the topic
+# const waiter = order.creator;
+# const initialTime = order.createdAt;
+# const closeTime = order.closedAt;
+
+# const mappedOrder = order.OrderProduct.map((item) => {
+#   return {
+#     name: item.product.name,
+#     amount: item.amount,
+#     price: item.product.price,
+#     total: round2(round2(item.product.price) * item.amount),
+#   };
+# });
+
+
+def print_order(order):
+    print(f"Atendido por: {order['waiter']}")
+    print("Abertura: " + order["openTime"])
+    print("Fecho: " + order["closeTime"])
+    print("\nCompras: ")
+    for item in order["order"]:
+        # format the string to have 5 characters aligned right with spaces before
+        item_amount = "{:>2}".format(item["amount"])
+
+        # item_amount = "{:.2f}".format(item["amount"])
+        item_name = "{:<20}".format(item["name"])
+        item_total = "{:>5}".format(item["total"])
+        item_price = "{:>4}".format(item["price"])
+        print(
+            f"{item_name}      {item_amount} x {item_price}      {item_total}€")
+
+    total = "{:>5}".format(order["total"])
+    print(f"\n\nTotal: {total}")
+
+
 try:
-  while True:
-    msg = consumer.poll(1.0)
-    if msg is not None and msg.error() is None:
-      json_data = json.loads(msg.value().decode('utf-8'))
-      print(json_data)
+    while True:
+        msg = consumer.poll(1.0)
+        if msg is not None and msg.error() is None:
+            json_data = json.loads(msg.value().decode('utf-8'))
+            print(json_data)
+
+            print_order(json_data)
+
 except KeyboardInterrupt:
-  pass
+    pass
 finally:
-      consumer.close()
+    consumer.close()
