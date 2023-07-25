65da0fdafd870c29a7d38f7863d71fa9bb4e7d7a
André Moreira <andre.moreira.9@hotmail.com>
2023-07-26 00:33:55 +0100

    Make api call dynamic

--- src/app/api/kafka/route.ts
+++ src/app/api/kafka/route.ts
@@ -1,7 +1,10 @@
 import { OrderPrintController } from "@/controllers/OrderPrintController";
+import { setDynamicRoute } from "@/helpers/api";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(request: Request) {
+  setDynamicRoute(request);
+
   const result = await OrderPrintController.GenerateOrder();
   return NextResponse.json({ result });
 }
