d4094c77fc69d0190eedd2468e9ae5770460d7f8
André Moreira <andre.moreira.9@hotmail.com>
2023-11-29 19:40:18 +0100

    Add comment on route

--- src/app/api/orders/[id]/request/route.ts
+++ src/app/api/orders/[id]/request/route.ts
@@ -2,6 +2,9 @@ import { TableController } from "@/controllers/TableControllers";
 import { withApiAuthRequired } from "@auth0/nextjs-auth0";
 import { NextRequest, NextResponse } from "next/server";
 
+/**
+ * @description This API route is used to get the printable order products from the print page
+ */
 const GET = withApiAuthRequired(async (_, { params }) => {
   const idRaw = params?.id as string;
 
